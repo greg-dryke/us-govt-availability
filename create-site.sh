4042050b5f7159725a593924d7cde5b7f9eff034
Greg Dryke <gdryke@github.com>
2020-12-20 23:03:22 -0600

    color

--- create-site.sh
+++ create-site.sh
@@ -14,4 +14,6 @@ days_down=$(curl -s $down_days_url)
 
 avail=$(perl -e "printf \"%.7f\", ((($days_since-$days_down) / $days_since) * 100.0000000)")
 
-echo $content | sed  s/%%AVAILABILITY%%/$avail/g > $index
+color=$(perl -e "if ($avail >= 99.99){print 'green'} else {print 'red'}"
+
+echo $content | sed  s/%%AVAILABILITY%%/$avail/g | set s/%%color%%/$color/g > $index

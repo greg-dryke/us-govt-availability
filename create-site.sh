46d78ee3941e837ac142e07b93584b3d2c601b9c
Greg Dryke <gdryke@github.com>
2020-12-20 22:43:15 -0600

    give up

--- create-site.sh
+++ create-site.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-set -v
-
-echo "Result: "$(command -v gdate)
-
-if ! command -v gdate &> /dev/null
-then
-    alias gdate='date'
-fi
-
 template="index.html_template"
 index="index.html"
 down_days_url="https://raw.githubusercontent.com/greg-dryke/us-govt-availability/trunk/down_days.txt"
 
 content=$(cat $template)
 
-now=$(gdate +%s)
-govt_start=$(gdate -d 'dec 1 1789' +%s)
+now=$(date +%s)
+govt_start=$(date -d 'dec 1 1789' +%s)
 days_since=$(( (now - govt_start) / 86400 ))
 
 days_down=$(curl -s $down_days_url)
